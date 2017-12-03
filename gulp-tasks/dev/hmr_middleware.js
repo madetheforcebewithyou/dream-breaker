ffefef909f677d6ab8455d95fc9f7c4ba25b4761
bob <bob@vynca.org>
2017-12-03 13:08:04 +0800

    Renamed webpack_hmr_config to webpack_config

--- gulp-tasks/dev/hmr_middleware.js
+++ gulp-tasks/dev/hmr_middleware.js
@@ -1,13 +1,13 @@
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackHmrConfig from './webpack_hmr_config.js';
+import webpackConfig from './webpack_config.js';
 
-const compiler = webpack(webpackHmrConfig);
+const compiler = webpack(webpackConfig);
 
 export default [
   webpackDevMiddleware(compiler, {
-    publicPath: webpackHmrConfig.output.publicPath,
+    publicPath: webpackConfig.output.publicPath,
     stats: {
       colors: true,
       chunks: false,
